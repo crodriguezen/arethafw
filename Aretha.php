a16e37d3d76566cfadbb7379110a2cc03f77134d
Cristian Rodríguez <crodriguezen@gmail.com>
2020-11-20 11:50:11 -0600

    Update Aretha.php
    Add function startsWith

--- Aretha.php
+++ Aretha.php
@@ -875,6 +875,10 @@ class Aretha {
 	    return $d && $d->format($format) == $date;
 	}
 
+	public static function startsWith($haystack, $needle) {
+    	return substr($haystack, 0, strlen($needle)) === $needle;
+	}
+
 	public static function endsWith($haystack, $needle) {
 		return substr($haystack, -strlen($needle)) === $needle;
 	}
